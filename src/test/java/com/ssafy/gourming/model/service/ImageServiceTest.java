a034a0eabab25de725d9c0ef5f991933a1fdc7a1
sje <sje5149@naver.com>
2026-06-25 10:46:25 +0900

    test: SpringBootTest에서 비밀번호 재설정 메일 비활성화

--- src/test/java/com/ssafy/gourming/model/service/ImageServiceTest.java
+++ src/test/java/com/ssafy/gourming/model/service/ImageServiceTest.java
@@ -14,7 +14,7 @@ import static org.mockito.Mockito.*;
 import org.springframework.mock.web.MockMultipartFile;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
+@SpringBootTest(properties = "password-reset.mail.enabled=false")
 public class ImageServiceTest {
 
     @Autowired

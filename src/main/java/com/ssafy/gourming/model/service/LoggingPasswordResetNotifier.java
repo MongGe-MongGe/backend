7010e8dfa8d4c7fa41483429518273e0725b97de
sje <sje5149@naver.com>
2026-06-25 09:51:57 +0900

    refactor: 비밀번호 재설정 notifier 조건부 등록 적용

--- src/main/java/com/ssafy/gourming/model/service/LoggingPasswordResetNotifier.java
+++ src/main/java/com/ssafy/gourming/model/service/LoggingPasswordResetNotifier.java
@@ -3,6 +3,7 @@ package com.ssafy.gourming.model.service;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -10,6 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+// 실제 메일 발송이 꺼진 기본/개발 환경에서는 reset link를 로그로 남긴다.
+@ConditionalOnProperty(
+		name = "password-reset.mail.enabled",
+		havingValue = "false",
+		matchIfMissing = true
+)
 public class LoggingPasswordResetNotifier implements PasswordResetNotifier {
 
 	@Value("${password-reset.frontend-base-url:http://localhost:5173/reset-password}")
